b6f1d9c5e402ff6db12299195b7a440aeaaa7ef0
Nahidul Islam Shanto <nahidulislamshantogo@gmail.com>
2023-09-23 02:16:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Full Stack Developer" , "Android App Developer" , "SCO Maintain"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
